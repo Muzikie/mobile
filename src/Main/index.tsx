9210dfbd6eca0a828086f4f86e20a25645664fec
reyraa <ali@muzikie.com>
2023-10-08 23:06:54 +0200

    Add routes

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -15,26 +15,58 @@ function Main(): JSX.Element {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home" tabBar={BottomTabBar}>
+      <Tab.Navigator initialRouteName="Submit" tabBar={BottomTabBar}>
         <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{tabBarShowLabel: false, tabBarActiveTintColor: '#09c', headerStyle: {backgroundColor: '#FEEAE3', shadowColor: 'transparent', elevation: 0}}}
+          options={{
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#09c',
+            headerStyle: {
+              backgroundColor: '#FEEAE3',
+              shadowColor: 'transparent',
+              elevation: 0,
+            },
+          }}
         />
         <Tab.Screen
           name="Login"
           component={LoginScreen}
-          options={{tabBarShowLabel: false, tabBarActiveTintColor: '#09c', headerStyle: {backgroundColor: '#FEEAE3', shadowColor: 'transparent', elevation: 0}}}
+          options={{
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#09c',
+            headerStyle: {
+              backgroundColor: '#FEEAE3',
+              shadowColor: 'transparent',
+              elevation: 0,
+            },
+          }}
         />
         <Tab.Screen
           name="Profile"
           component={ProfileScreen}
-          options={{tabBarShowLabel: false, tabBarActiveTintColor: '#09c', headerStyle: {backgroundColor: '#FEEAE3', shadowColor: 'transparent', elevation: 0}}}
+          options={{
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#09c',
+            headerStyle: {
+              backgroundColor: '#FEEAE3',
+              shadowColor: 'transparent',
+              elevation: 0,
+            },
+          }}
         />
         <Tab.Screen
           name="Submit"
           component={SubmitScreen}
-          options={{tabBarShowLabel: false, tabBarActiveTintColor: '#09c', headerStyle: {backgroundColor: '#FEEAE3', shadowColor: 'transparent', elevation: 0}}}
+          options={{
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#09c',
+            headerStyle: {
+              backgroundColor: '#FEEAE3',
+              shadowColor: 'transparent',
+              elevation: 0,
+            },
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
