c02eeec6e16ea4fe1623d88feececcc8d86784d4
reyraa <ali@muzikie.com>
2023-10-11 22:35:44 +0200

    Remove bignumber

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,19 +1,21 @@
-import BigNumber from 'bignumber.js';
+// import BigNumber from 'bignumber.js';
 
 export const formatThousands = (num: number): string => {
   const si = [
-    { value: 1E18, sign: 'E' },
-    { value: 1E15, sign: 'P' },
-    { value: 1E12, sign: 'T' },
-    { value: 1E9, sign: 'B' },
-    { value: 1E6, sign: 'M' },
-    { value: 1E3, sign: 'K' },
+    {value: 1e18, sign: 'E'},
+    {value: 1e15, sign: 'P'},
+    {value: 1e12, sign: 'T'},
+    {value: 1e9, sign: 'B'},
+    {value: 1e6, sign: 'M'},
+    {value: 1e3, sign: 'K'},
   ];
 
-  const signItem = si.find((item) => (num >= item.value));
+  const signItem = si.find(item => num >= item.value);
   return !signItem
     ? num.toString()
-    : (num / signItem.value).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, '$1') + signItem.sign;
+    : (num / signItem.value)
+        .toFixed(2)
+        .replace(/\.0+$|(\.[0-9]*[1-9])0+$/, '$1') + signItem.sign;
 };
 
 export const getYear = (num: number): string => {
@@ -21,13 +23,16 @@ export const getYear = (num: number): string => {
   return year.toString();
 };
 
-export const fromBaseToken = (num: string, token?: string|undefined): string => {
-  const formatted = BigNumber(num).dividedBy(BigNumber(1E8)).toFixed(4);
-  return token ? `${formatted} ${token}` : formatted;
-};
+// export const fromBaseToken = (
+//   num: string,
+//   token?: string | undefined,
+// ): string => {
+//   const formatted = BigNumber(num).dividedBy(BigNumber(1e8)).toFixed(4);
+//   return token ? `${formatted} ${token}` : formatted;
+// };
 
-export const toBaseToken = (num: string): string =>
-  (BigNumber(num).multipliedBy(BigNumber(1E8))).toFixed(0);
+// export const toBaseToken = (num: string): string =>
+//   BigNumber(num).multipliedBy(BigNumber(1e8)).toFixed(0);
 
 export const truncateAddress = (address: string): string => {
   return address.replace(/^(.{6})(.+)?(.{5})$/, '$1...$3');
