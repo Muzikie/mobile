d59a643c6eef0c88441e22f65702e1ef84cad2bd
reyraa <ali@muzikie.com>
2023-12-07 23:31:03 +0100

    Add dark mode colors

--- src/config/stylesGuides/colors.ts
+++ src/config/stylesGuides/colors.ts
@@ -1,4 +1,11 @@
 const common = {
+  backdrop: 'transparent',
+  iceberg: '#D7F4EE',
+  lavender: '#8A80BF',
+};
+
+const light = {
+  ...common,
   black: '#000',
   white: '#fff',
   beige: '#FEEAE3',
@@ -7,8 +14,6 @@ const common = {
   lightGrey: '#BDB3B5',
   paleGrey: '#E8E7E7',
   darkGrey: '#98918F',
-  iceberg: '#D7F4EE',
-  lavender: '#8A80BF',
   fadeBeige: '#FEF2EE',
   success: '#42BFC7',
   fadeSuccess: '#BDDFE1',
@@ -17,15 +22,26 @@ const common = {
   lemon: '#FDE8B2',
   navyBlue: '#88A4C9',
   opaqueWhite: 'rgba(255, 255, 255, 0.5)',
-  backdrop: 'transparent',
-};
-
-const light = {
-  ...common,
 };
 
 const dark = {
   ...common,
+  black: '#fff',
+  white: '#000',
+  beige: '#363636',
+  purple: '#A687AB',
+  grey: '#BDB3B5',
+  lightGrey: '#98918F',
+  paleGrey: '#595857',
+  darkGrey: '#BDB3B5',
+  fadeBeige: '#595857',
+  success: '#378B90',
+  fadeSuccess: '#3C5E60',
+  error: '#B97A42',
+  errorFade: '#714D51',
+  lemon: '#9F8F67',
+  navyBlue: '#6C7F98',
+  opaqueWhite: 'rgba(30, 30, 30, 0.5)',
 };
 
 export default {
